2c09a211aa508fe535cc2d78c721cdd0796d6720
theea <theea@DESKTOP-TM9K5EJ>
2024-11-27 21:24:58 +0100

    Add project files.

---
+++ Handling_Multiple_Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Handling_Multiple_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HandleMultipleExceptions("2" ,2);
+            HandleMultipleExceptions("A" ,3);
+            HandleMultipleExceptions("2" ,4);
+        }
+
+        private static void HandleMultipleExceptions(string inputString, int arrayIndex )
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            try
+            {
+                int parsednumber = Int32.Parse(inputString);
+                int testIndexNumber = numbers[arrayIndex];
+
+                Console.WriteLine("Correct input");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format");
+            }
+            catch (IndexOutOfRangeException) 
+            {
+                Console.WriteLine("Index out of range");
+            }
+        }
+    }
+}
